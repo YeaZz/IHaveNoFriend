eab4801a731ad446130203625f5ad21f47f54658
YeaZz <perhapsitisyeazz@gmail.com>
2020-08-24 21:33:28 +0200

    Update

--- src/main/java/com/gmail/perhapsitisyeazz/util/Message.java
+++ src/main/java/com/gmail/perhapsitisyeazz/util/Message.java
@@ -2,7 +2,11 @@ package com.gmail.perhapsitisyeazz.util;
 
 import com.gmail.perhapsitisyeazz.IHaveNoFriend;
 import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.chat.*;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
